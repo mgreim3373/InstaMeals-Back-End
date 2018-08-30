9ac3fc1e492ed4aa271194560acc8ba8fc9d332b
mgreim3373 <mgreim3373@gmail.com>
2018-08-30 10:40:58 -0400

    Remove Views Folder

---
+++ app/serializers/recipe_serializer.rb
@@ -0,0 +1,4 @@
+class RecipeSerializer < ActiveModel::Serializer
+  attributes :id, :prep_time, :cook_time, :serving_size, :pot_mode, :pot_pressure, :ingredient, :prep_instruction
+  has_one :user_id
+end
