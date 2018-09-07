e89d1f50cfb0f4381af7c5c39879419c8b077ecf
mgreim3373 <mgreim3373@gmail.com>
2018-09-06 21:29:51 -0400

    Hid Edit and Delete Buttons -Hid edit and delete buttons for non-owners

--- app/serializers/recipe_serializer.rb
+++ app/serializers/recipe_serializer.rb
@@ -1,4 +1,8 @@
 class RecipeSerializer < ActiveModel::Serializer
-  attributes :name, :id, :prep_time, :cook_time, :serving_size, :pot_mode, :pot_pressure, :ingredient, :prep_instruction, :photo
+  attributes :name, :id, :prep_time, :cook_time, :serving_size, :pot_mode, :pot_pressure, :ingredient, :prep_instruction, :photo, :editable
   has_one :user
+
+  def editable
+    scope == object.user
+  end
 end
